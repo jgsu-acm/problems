2b847bca71a50b82fafc8c9d5e13b4a87a005506
AgOH <stagoh@qq.com>
2022-01-27 00:09:30 +0800

    manager: use sa problem cfg template

--- manager.py
+++ manager.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup
 PATH_GENTP_CPP = Path("template/gen_tp.cpp")
 PATH_GENTP_PY = Path("template/gen_tp.py")
 PATH_PROTP = Path("template/pro_tp.md")
+PATH_SACFG_TP = Path("template/cfg_sa_tp.yaml")
 PATH_SAPROTP = Path("template/pro_sa_tp.md")
 PATH_STDTP = Path("template/std_tp.cpp")
 PATH_TMP_FOLDER = Path("tmp")
@@ -124,6 +125,7 @@ def create(args):
     elif args.submitans:
         for problem in problems:
             shutil.copy(PATH_SAPROTP, get_pro_path(problem) / f"{problem}.md")
+            shutil.copy(PATH_SACFG_TP, get_pro_path(problem) / "config.yaml")
     else:
         for problem in problems:
             shutil.copy(PATH_PROTP, get_pro_path(problem) / f"{problem}.md")
