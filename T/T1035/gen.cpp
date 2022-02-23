ec2db35fd73759ca1e83a740e84c93aa01e55d8d
AgOH <stagoh@qq.com>
2022-02-23 21:30:06 +0800

    problem(T1035): create

---
+++ T/T1035/gen.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <ctime>
+using namespace std;
+typedef uniform_int_distribution<> rnd;
+typedef uniform_real_distribution<> rndf;
+mt19937 egn(time(nullptr));
+const int CASES = 10;
+// ==============================
+#include <string>
+// ==============================
+int main(int argc, char const* argv[])
+{
+    for(int t=1;t<=CASES;t++)
+    {
+        ofstream fout(to_string(t)+".in");
+        // ==============================
+        string s2;
+        int len = rnd(1,10)(egn);
+        for(int i=0;i<len;i++) s2.push_back((char)(rnd('A','Z')(egn)));
+        string s1;
+        while(s1.length()+s2.length()<=1e6)
+        {
+            int p = rnd(1,100)(egn);
+            if(p<=10) s1.push_back((char)(rnd('A','Z')(egn)));
+            else 
+            {
+                s1+=s2;
+                if(p>=50) s1.back() = (char)(rnd('A','Z')(egn));
+            }
+        }
+        fout<<s1<<endl<<s2<<endl;
+        // ==============================
+        fout.close();
+    }
+    return 0;
+}
---
+++ T/T1035/std.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int maxn = 1e6 + 5;
+int nxt[maxn];
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (size_t i = 1, j = 0; i < s2.size(); i++)
+    {
+        while (j > 0 && s2[j] != s2[i])
+            j = nxt[j - 1];
+        if (s2[j] == s2[i])
+            nxt[i] = ++j;
+    }
+    for (size_t i = 0, j = 0; i < s1.size();)
+    {
+        if (s1[i] == s2[j])
+        {
+            i++, j++;
+            if (j != s2.size())
+                continue;
+            cout << i - s2.size() + 1 << ' ';
+        }
+        j = nxt[j - 1];
+        if (s1[i] != s2[j])
+            i++;
+    }
+    return 0;
+}
