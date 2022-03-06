c4b47f63dbfc7cc500145ffa6aa881a42fb5d68a
AgOH <stagoh@qq.com>
2022-03-06 20:28:13 +0800

    manager: fix bug without spids

--- manager.py
+++ manager.py
@@ -48,23 +48,25 @@ def process_ids(items: list[str]):
 
 @click.command("c", short_help="创建题目", help=f"创建 PIDS 题目\n\n{ID_SYNTAX_HELP.format(ids='PIDS 与 SOURCEIDS')}")
 @click.argument("pids", nargs=-1, required=True)
+@click.option("--spids", "-i", multiple=True, metavar="SOURCEIDS", help="获取哪些题面，数量及顺序要与欲创建的题目相同")
 @click.option("--source", "-s", type=click.Choice(["luogu"]), default="luogu",
               callback=lambda c, p, v: v if c.params["spids"] else None, help="从何处获取题目")
-@click.option("--spids", "-i", multiple=True, metavar="SOURCEIDS", help="获取哪些题面，数量及顺序要与欲创建的题目相同")
 @click.option("--submitans", "-sa", "is_sa", is_flag=True, help="提交答案题")
 @click.option("--nogen", "-ng", is_flag=True, help="不生成生成器模板")
 @click.option("--nostd", "-ns", is_flag=True, help="不生成标程模板")
 @click.option("--python", "-py", "use_python", is_flag=True, help="使用 python 生成器")
-def create(pids: list[str], source: str, spids: list[str], is_sa: bool, nogen: bool, nostd: bool, use_python: bool):
+def create(pids: list[str], spids: list[str], source: str, is_sa: bool, nogen: bool, nostd: bool, use_python: bool):
     logger = logging.getLogger("创建题目")
     logger.debug(f"题目：{pids}，来源：[{source}]{spids}"
                  f"是否是提交答案题：{is_sa}，是否创建数据生成器：{not nogen}，是否创建标程：{not nostd}，"
                  f"是否使用 python 数据生成器：{use_python}")
 
     pids, spids = process_ids(pids), process_ids(spids)
-    logger.info(f"欲创建的题目 ID 为：{pids}；题目来源 ID 为：{spids}")
-    if len(pids) != len(spids):
-        raise Exception("题目数量不匹配")
+    logger.info(f"欲创建的题目 ID 为：{pids}")
+    if spids:
+        logger.info("题目来源 ID 为：{spids}")
+        if source and len(pids) != len(spids):
+            raise Exception("题目数量不匹配")
     args = is_sa, nogen, nostd, use_python
     iterator = iter(zip(pids, spids if spids else repeat(None)))
     problem_map.get(source, Creator)(*next(iterator), *args).create().open()
