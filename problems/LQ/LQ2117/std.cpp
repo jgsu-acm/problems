54e2cc9651bee9e209deef09739976cbf692708a
AgOH <stagoh@qq.com>
2022-03-06 21:01:52 +0800

    problem(LQ2117): create

---
+++ problems/LQ/LQ2117/std.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+//==========================================
+const int maxn = 1e5+5;
+signed main(signed argc, char const *argv[])
+{
+#ifdef LOCAL
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    clock_t c1 = clock();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //======================================
+    int cnt = 0;
+    for(int i=1;i<=2021;i++)
+        for(int j=1;j<=2021;j++)
+            if(i*j<=2021) ++cnt;
+    cout<<cnt<<endl;
+    //======================================
+#ifdef LOCAL
+    cerr << "Time Used:" << clock() - c1 << "ms" << endl;
+#endif
+    return 0;
+}
