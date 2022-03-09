a7ca3ac419ace11e61015e2193eb8d060765f6e5
AgOH <stagoh@qq.com>
2022-03-09 12:45:35 +0800

    problem(LQ2123): enhance gen

--- problems/LQ/LQ2123/gen.py
+++ problems/LQ/LQ2123/gen.py
@@ -1,17 +1,30 @@
+from math import floor
 from cyaron import *
 from random import randint
+from sympy import randprime
 
 CASES = 10
 
 for t in range(1, CASES + 1):
     io = IO(f"{t}.in")
     # ==============================
-    p = randint(1, 10)
-    if p <= 3:
-        n = randint(1, int(1e12))
+    if t == 1:
+        n = 1
+    elif t == 2:
+        n = 400
+    elif t == 3:
+        n = 551
     else:
-        k = randint(1, int(1e3))
-        n = randint(1, int(1e6)) * k * k
+        if t <= 6:
+            maxn = 10**8
+        else:
+            maxn = 10**12
+        if t % 3 == 0:
+            n = randprime(1, maxn)
+        else:
+            s = floor(sqrt(sqrt(maxn)))
+            k = randint(1, s)
+            n = randint(1, s*s) * k * k
     io.input_writeln(n)
     # ==============================
     io.close()
