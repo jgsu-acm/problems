5f3ce13a4c0ed9c0a0b5cb4ebacd8b4d3f4d384b
AgOH <stagoh@qq.com>
2022-03-25 13:06:59 +0800

    problem(LQ2126): update std

--- problems/LQ/LQ2126/std.cpp
+++ problems/LQ/LQ2126/std.cpp
@@ -13,7 +13,21 @@ ll qpow(ll a,ll k)
         if(k&1) res=res*a%mod;
     return res;
 }
-ll fct[maxn], inv[maxn], s[maxn];
+ll fct[maxn]={0,1}, inv[maxn]={0,1};
+inline void inv1()  //? 快速幂 O(nlogn) 求逆元
+{
+    for(int i=2;i<=1e6;i++) inv[i]=qpow(fct[i], mod-2);
+}
+ll s[maxn], sv[maxn];
+inline void inv2()  //? 线性求任意 n 个数的逆元
+{
+    int n = 1e6;
+    s[1]=1;
+    for(int i=2;i<=n;i++) s[i]=s[i-1]*fct[i]%mod;
+    sv[n]=qpow(s[n], mod-2);
+    for(int i=n-1;i>=1;i--) sv[i]=sv[i+1]*fct[i+1]%mod;
+    for(int i=2;i<=n;i++) inv[i]=sv[i]*s[i-1]%mod;
+}
 signed main(signed argc, char const *argv[])
 {
 #ifdef LOCAL
@@ -24,15 +38,8 @@ signed main(signed argc, char const *argv[])
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     //======================================
-    fct[1]=inv[1]=1;
-    for(int i=2;i<=1e6;i++)
-    {
-        fct[i]=fct[i-1]*i%mod;
-        inv[i]=qpow(fct[i], mod-2);
-    }
-    s[2]=4;
-    for(int i=3;i<=20;i++)
-        s[i]=((1<<(i-1))*(3*i-4)-i+2)%mod;
+    for(int i=2;i<=1e6;i++) fct[i]=fct[i-1]*i%mod;
+    inv2();  // 使用线性求任意 n 个数的逆元
     int t;
     cin>>t;
     while(t--)
@@ -46,7 +53,7 @@ signed main(signed argc, char const *argv[])
             ll a = fct[n]*inv[len]%mod;
             ll c1 = a*((1<<len)-1)%mod;
             if(n<3*(1<<(len-2))) cout<<c1<<'\n';
-            else cout<<(c1+a*s[len])%mod<<'\n';
+            else cout<<(c1+a*((1<<(len-1))*(3*len-4)-len+2))%mod<<'\n';
         }
     }
     //======================================
