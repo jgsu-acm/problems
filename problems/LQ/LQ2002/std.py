3c59f7e3fc5bce76628ab33eee2abb7cfad3a157
AgOH <stagoh@qq.com>
2022-03-29 12:47:56 +0800

    problem(LQ2002): update std

--- problems/LQ/LQ2002/std.py
+++ problems/LQ/LQ2002/std.py
@@ -1,9 +1,3 @@
-from datetime import date, timedelta
+from datetime import date
 
-d1 = date(1921, 7, 23)
-d2 = date(2020, 7, 1)
-cnt = 0
-while d1 < d2:
-    cnt += 24*60
-    d1 += timedelta(days=1)
-print(cnt)
+print((date(2020, 7, 1)-date(1921, 7, 23)).days*24*60)
