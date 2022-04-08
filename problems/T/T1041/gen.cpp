3b8f43c8de2bf2d22c9c3299d6ac2000ae88ff98
AgOH <stagoh@qq.com>
2022-04-08 18:23:47 +0800

    problem(T1041): narrow data range

--- problems/T/T1041/gen.cpp
+++ problems/T/T1041/gen.cpp
@@ -5,9 +5,22 @@ typedef uniform_int_distribution<> rnd;
 typedef uniform_int_distribution<ll> rndll;
 typedef uniform_real_distribution<> rndf;
 mt19937 egn(time(nullptr));
-const int CASES = 10;
+const int CASES = 25;
 // ==============================
-
+ll phi(ll x)
+{
+    ll res = x;
+    for(ll i=2;i*i<=x;i++)
+    {
+        if(x%i==0)
+        {
+            res = res/i*(i-1);
+            while(x%i==0) x/=i;
+        }
+    }
+    if(x>1) res = res/x*(x-1);
+    return res;
+}
 // ==============================
 int main(int argc, char const* argv[])
 {
@@ -17,7 +30,7 @@ int main(int argc, char const* argv[])
         // ==============================
         int a = rnd(1,1e9)(egn);
         int p = rnd(1,1e9)(egn);
-        if(_t>5)
+        if(_t>15)
         {
             while(__gcd(a,p)==1)
             {
@@ -26,8 +39,13 @@ int main(int argc, char const* argv[])
             }
         }
         string b;
-        generate_n(inserter(b, b.begin()), 2e7-1, []() { return char(rnd('0','9')(egn)); });
-        b = char(rnd('0','9')(egn)) + b;
+        if(_t<=5)
+        {
+            int tmp = rnd(1,phi(p))(egn);
+            b = to_string(tmp);
+        }
+        else generate_n(inserter(b, b.begin()), 1e7-1, []() { return char(rnd('0','9')(egn)); });
+        b = char(rnd('1','9')(egn)) + b;
         fout<<a<<' '<<b<<' '<<p<<endl;
         // ==============================
         fout.close();
