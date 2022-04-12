a4d192572e7bd5c5160c2759e07fe0df4b98a294
AgOH <stagoh@qq.com>
2022-04-12 14:08:35 +0800

    problem(LQ2115): add std

---
+++ problems/LQ/LQ2115/std.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+//==========================================
+signed main(signed argc, char const *argv[])
+{
+#ifdef LOCAL
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    auto c1 = chrono::high_resolution_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //======================================
+    cout<<2021%20<<endl;
+    //======================================
+#ifdef LOCAL
+    auto c2 = chrono::high_resolution_clock::now();
+    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(c2-c1).count()<<"ms"<<endl;
+#endif
+    return 0;
+}
