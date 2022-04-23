e5d4590c63fe2ec0f34d1bc106fe22446b2d6f38
AgOH <stagoh@qq.com>
2022-04-23 19:27:01 +0800

    problem(LQ2213): enhance gen

--- problems/LQ/LQ2213/gen.cpp
+++ problems/LQ/LQ2213/gen.cpp
@@ -16,33 +16,40 @@ int main(int argc, char const* argv[])
         ofstream fout(to_string(_t)+".in");
         // ==============================
         ll a,b,n,t;
-        if(_t<=5)
-        {
-            a = rnd(1,1e4)(egn);
-            b = rnd(1,1e4)(egn);
-        }
-        else
-        {
-            a = rndll(1,1e16)(egn);
-            b = rndll(1,1e16)(egn);
-        }
+        a = rnd(5,10)(egn);
+        b = rnd(5,10)(egn);
         t = 5*a+2*b;
-        switch((_t-1)%5)
+        switch(_t)
         {
-        case 0:
-            n = t*rnd(1,10)(egn);
-            break;
         case 1:
-            n = t*rnd(1,10)(egn)+rnd(1,5)(egn)*a;
+            n = t*rnd(1,1e4)(egn);
             break;
         case 2:
-            n = t*rnd(1,10)(egn)+rnd(1,4)(egn)*a+rnd(1,a-1)(egn);
+            n = t*rnd(1,1e4)(egn)+rnd(1,5)(egn)*a;
             break;
         case 3:
-            n = t*rnd(1,10)(egn)+5*a+rnd(1,2)(egn)*b;
+            n = t*rnd(1,1e4)(egn)+rnd(1,4)(egn)*a+rnd(1,a-1)(egn);
             break;
         case 4:
-            n = t*rnd(1,10)(egn)+5*a+b+rnd(1,b-1)(egn);
+            n = t*rnd(1,1e4)(egn)+5*a+rnd(1,2)(egn)*b;
+            break;
+        case 5:
+            n = t*rnd(1,1e4)(egn)+5*a+b+rnd(1,b-1)(egn);
+            break;
+        case 6:
+            n = t*rndll(1e15, 5e15)(egn)+rnd(1,4)(egn)*a+rnd(1,a-1)(egn);
+            break;
+        case 7:
+            n = t*rndll(1e15, 5e15)(egn)+5*a+b+rnd(1,b-1)(egn);
+            break;
+        case 8:
+            n = t*rndll(1e15, 5e15)(egn)+5*a+b+rnd(1,b-1)(egn);
+            break;
+        case 9:
+            n = t*rndll(1e15, 5e15)(egn)+rnd(1,4)(egn)*a+rnd(1,a-1)(egn);
+            break;
+        case 10:
+            n = t*rndll(1e15, 5e15)(egn)+5*a+b+rnd(1,b-1)(egn);
             break;
         }
         fout<<a<<' '<<b<<' '<<n<<endl;
