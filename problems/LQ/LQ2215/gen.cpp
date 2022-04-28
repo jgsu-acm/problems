d745e804d942649e824a0d69a2b29254e1f3ca11
AgOH <stagoh@qq.com>
2022-04-28 20:18:24 +0800

    problem(LQ2215): enhance gen

--- problems/LQ/LQ2215/gen.cpp
+++ problems/LQ/LQ2215/gen.cpp
@@ -5,9 +5,9 @@ typedef uniform_int_distribution<> rnd;
 typedef uniform_int_distribution<ll> rndll;
 typedef uniform_real_distribution<> rndf;
 mt19937 egn(time(nullptr));
-const int CASES = 10;
+const int CASES = 20;
 // ==============================
-
+const int mod = 1e9+7;
 // ==============================
 int main(int argc, char const* argv[])
 {
@@ -19,67 +19,85 @@ int main(int argc, char const* argv[])
         if(_t==1)
         {
             n = 2;
-            ma = rnd(1,8)(egn);
-            mb = rnd(1,8)(egn);
+            do
+            {
+                ma = rnd(5,8)(egn);
+                mb = rnd(5,8)(egn);
+            } while(ma==mb);
         }
         else if(_t==2)
         {
-            n = rnd(5,10)(egn);
+            n = 2;
             ma = mb = rnd(5,8)(egn);
         }
-        else if(_t==3)
+        else if(_t<=4)
         {
             n = 10;
             ma = mb = 8;
         }
         else if(_t<=6)
         {
-            n = rnd(2,1000)(egn);
-            ma = rnd(9,1e5)(egn);
-            mb = rnd(9,1e5)(egn);
+            n = 10;
+            ma = rnd(5,8)(egn);
+            mb = rnd(5,8)(egn);
         }
-        else if(_t<=7)
+        else if(_t==7)
         {
             n = 2;
-            ma = rnd(9,1e5)(egn);
-            mb = rnd(9,1e5)(egn);
+            do
+            {
+                ma = rnd(8e4,1e5)(egn);
+                mb = rnd(8e4,1e5)(egn);
+            } while(ma==mb);
         }
-        else if(_t<=8)
+        else if(_t==8)
         {
-            n = rnd(2,1000)(egn);
-            ma = mb = rnd(9,1e5)(egn);
+            n = 2;
+            ma = mb = rnd(8e4,1e5)(egn);
         }
-        else 
+        else if(_t<=10)
         {
             n = 1000;
             ma = mb = 1e5;
         }
+        else
+        {
+            n = 1000;
+            ma = rnd(8e4,1e5)(egn);
+            mb = rnd(8e4,1e5)(egn);
+        }
         fout<<n<<endl;
         if(ma<mb) swap(ma, mb);
         vector<int> v1,v2;
-        if(_t==2||_t==8)
+        if(_t==4||_t==10)
         {
-            int p = rnd(1,ma)(egn);
-            for(int i=0;i<p;i++)
+            int x = rnd(1,n-1)(egn);
+            v1.push_back(x);
+            v2.push_back(x);
+            for(int i=1;i<ma;i++)
             {
-                int t = rnd(0,n-1)(egn);
-                v1.push_back(t);
-                v2.push_back(t);
-            }
-            for(int i=0;i<ma-p;i++)
-            {
-                v1.push_back(rnd(0,n-1)(egn));
-                v2.push_back(rnd(0,n-1)(egn));
+                x = rnd(0,n-1)(egn);
+                v1.push_back(x);
+                v2.push_back(x);
             }
         }
         else 
         {
-            for(int i=0;i<mb;i++)
+            int x = rnd(1,n-1)(egn);
+            int y = rnd(1,n-1)(egn);
+            v1.push_back(x);
+            v2.push_back(y);
+            for(int i=1;i<mb;i++)
             {
                 int p = rnd(1,100)(egn);
-                if(p<=30)
+                if(p<=40)
                 {
                     v1.push_back(rnd(0,1)(egn));
+                    v2.push_back(rnd(0,n-1)(egn));
+                }
+                else if(p>60)
+                {
+                    v1.push_back(rnd(0,n-1)(egn));
                     v2.push_back(rnd(0,1)(egn));
                 }
                 else 
@@ -89,25 +107,23 @@ int main(int argc, char const* argv[])
                 }
             }
             generate_n(inserter(v1,v1.end()), ma-mb, [n]() { return rnd(0,n-1)(egn); });
-        }
-        if(ma==mb)
-        {
-            for(int i=0;i<ma;i++)
+            if(ma==mb)
             {
-                if(v1[i]>v2[i]) break;
-                if(v1[i]<v2[i])
+                for(int i=0;i<ma;i++)
                 {
-                    swap(v1[i], v2[i]);
-                    break;
+                    if(v1[i]>v2[i]) break;
+                    if(v1[i]<v2[i])
+                    {
+                        swap(v1[i], v2[i]);
+                        break;
+                    }
                 }
             }
         }
         fout<<ma<<endl;
-        for(auto i : v1) fout<<i<<' ';
-        fout<<endl;
+        for(auto i : v1) fout<<i<<' '; fout<<endl;
         fout<<mb<<endl;
-        for(auto i : v2) fout<<i<<' ';
-        fout<<endl;
+        for(auto i : v2) fout<<i<<' '; fout<<endl;
         // ==============================
         fout.close();
     }
