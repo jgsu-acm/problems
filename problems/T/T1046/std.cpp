3dafd3ef66addf0a5d417adea0cd2edcf854f1cd
StableAgOH <stagoh17@gmail.com>
2022-05-02 21:59:40 +0800

    problem(T1046): fix std

--- problems/T/T1046/std.cpp
+++ problems/T/T1046/std.cpp
@@ -1,46 +1,32 @@
 #include <iostream>
-#include <chrono>
-using namespace std;
-//==========================================
 #include <iomanip>
+#include <algorithm>
+#include <numeric>
+using namespace std;
 const int maxn = 1e5+5;
 const double eps = 1e-8;
-int n;
-double a[maxn], b[maxn];
+int n,k;
+int a[maxn], b[maxn];
+double c[maxn];
 bool check(double m)
 {
-    for(int i=1;i<=n;i++)
-        if(a[i]-m*b[i]>0)
-            return true;
-    return false;
+    for(int i=1;i<=n;i++) c[i] = a[i]-m*b[i];
+    sort(c+1, c+1+n, greater<double>());
+    return accumulate(c+1, c+1+k, 0.0)>0;
 }
-signed main(signed argc, char const *argv[])
+int main()
 {
-#ifdef LOCAL
-    freopen("in.in", "r", stdin);
-    freopen("out.out", "w", stdout);
-    auto c1 = chrono::high_resolution_clock::now();
-#endif
-#ifdef DEBUG
-    freopen("in.in", "r", stdin);
-#endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    //======================================
-    cin>>n;
+    cin>>n>>k;
     for(int i=1;i<=n;i++) cin>>a[i]>>b[i];
     double l=0, r=1e5;
     while(r-l>eps)
     {
-        double m = (l+r)/2;
-        if(check(m)) l = m;
-        else r = m;
+        double mid = (l+r)/2;
+        if(check(mid)) l = mid;
+        else r = mid;
     }
     cout<<fixed<<setprecision(6)<<l<<endl;
-    //======================================
-#ifdef LOCAL
-    auto c2 = chrono::high_resolution_clock::now();
-    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(c2-c1).count()<<"ms"<<endl;
-#endif
     return 0;
 }
