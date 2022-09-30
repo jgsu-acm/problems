8f2e64f530f9625711e03c0620eb888fe00e6cfb
Algor <1561601523@qq.com>
2022-09-30 20:04:05 +0800

    problem(A1034): create

---
+++ problems/A/A1034/gen.py
@@ -0,0 +1,10 @@
+from cyaron import *
+
+CASES = 10
+
+for _t in range(1, CASES + 1):
+    io = IO(f"{_t}.in")
+    # ==============================
+    io.input_writeln(_t*10-randint(0, 9))
+    # ==============================
+    io.close()
---
+++ problems/A/A1034/std.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+const double pi = 3.141592653589793;
+int main()
+{
+    int r;
+    cin >> r;
+    printf("%.6f\n", r*r*pi);
+    return 0;
+}
