632a029c4d446080d4f72538cfb4c45c02d62514
Algor <1561601523@qq.com>
2022-10-05 19:42:47 +0800

    problem(A1038): fixed std

--- problems/A/A1038/std.cpp
+++ problems/A/A1038/std.cpp
@@ -3,7 +3,8 @@ using namespace std;
 const int maxn = 85;
 long long f[maxn];
 long long fib(int n) {
-    if(n == 1 || n == 2) return 1;
+    if(n == 1) return 1;
+    if(n == 2) return 2;
     if(f[n]) return f[n];
     return f[n] = fib(n-1) + fib(n-2);
 }
