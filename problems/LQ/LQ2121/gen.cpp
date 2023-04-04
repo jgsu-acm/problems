0d5399b69fc899872d1fb243c252fb622fa3f29b
StableAgOH <stagoh17@gmail.com>
2023-04-04 20:20:46 +0800

    problem(LQ2121): enhance testcases

--- problems/LQ/LQ2121/gen.cpp
+++ problems/LQ/LQ2121/gen.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <random>
 #include <ctime>
+#include <sstream>
 using namespace std;
 typedef uniform_int_distribution<> rnd;
 typedef uniform_real_distribution<> rndf;
 mt19937 egn(time(nullptr));
-const int CASES = 10;
+const int CASES = 20;
 // ==============================
 
 // ==============================
@@ -16,17 +17,25 @@ int main(int argc, char const* argv[])
     {
         ofstream fout(to_string(t)+".in");
         // ==============================
-        for(int i=0;i<5;i++)
+        auto special = []()
         {
-            int p = rnd(1,10)(egn);
-            if(p<=4)
-            {
-                int x = rnd(1,9)(egn);
-                int y = rnd(0,8)(egn);
-                fout<<x<<y<<x<<y+1<<endl;
-            }
-            else fout<<rnd(1000,9999)(egn)<<endl;
+            stringstream ss;
+            int x = rnd(1,9)(egn);
+            int y = rnd(0,8)(egn);
+            ss<<x<<y<<x<<y+1;
+            return ss.str();
+        };
+        int k = (t-1)/4+1;
+        vector<string> v;
+        v.emplace_back(special());
+        for(int i=0;i<5-k;i++)
+        {
+            string s;
+            do s = to_string(rnd(1000,9999)(egn)); while(s[0]==s[2]&&s[1]+1==s[3]);
+            v.emplace_back(s);
         }
+        for(int i=0;i<k-1;i++) v.emplace_back(special());
+        for(auto&& s : v) fout<<s<<endl;
         // ==============================
         fout.close();
     }
